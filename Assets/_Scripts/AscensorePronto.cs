ed448bd03e5797db7107ce2a50bedfef978d6956
edoardohtml <bertazza.edoardo.studente@itispaleocapa.it>
2025-09-26 21:56:18 +0200

    !!prima di fare un commit su github eseguire il comando "git config --global core.autocrlf false" 2025-09-26 aggiornato alla versione 6.1, animazioni sistemate, aggiornato al nuovo sistema di input, utilizzo del sistema di renderizzazione URP.

---
+++ Assets/_Scripts/AscensorePronto.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AscensorePronto : StateMachineBehaviour {
+
+    private Transform Ascensore;
+    private Animator animPorta;
+    public float maxGazeTime = 3;
+    private float timer = 0;
+
+    // OnStateEnter is called before OnStateEnter is called on any state inside this state machine
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer = 0;
+        Ascensore = GameObject.Find("Ascensore").transform;
+        animPorta = Ascensore.Find("PortaAscensore").GetComponent<Animator>();
+    }
+
+    // OnStateUpdate is called before OnStateUpdate is called on any state inside this state machine
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        timer += Time.deltaTime;
+
+        if (timer > maxGazeTime)
+        {
+            animPorta.SetBool("open", true);
+        }
+
+    }
+
+    // OnStateExit is called before OnStateExit is called on any state inside this state machine
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        animPorta.SetBool("open", false);
+        timer = 0;
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    //
+    //}
+}
---
+++ Assets/_Scripts/apriPortaStart.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class apriPortaStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Animator>().SetBool("open", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
